348cef9b65c04afd22667c28309da52e72738de3
NDK1410 <khanh.nd1410@gmail.com>
2019-04-19 13:18:16 +0700

    devise-cancancan

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,11 @@
 class HomepagesController < ApplicationController
-  def index
+  before_action :check_user
+
+  def index; end
+
+  private
+
+  def check_user
+  	redirect_to new_user_session_path if !user_signed_in?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_020708) do
+ActiveRecord::Schema.define(version: 2019_04_19_081232) do
 
   create_table "categories", options: "ENGINE=MyISAM DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 2019_04_17_020708) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "word_answers", options: "ENGINE=MyISAM DEFAULT CHARSET=utf8", force: :cascade do |t|
