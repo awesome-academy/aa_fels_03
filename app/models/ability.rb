be0042d4551e3a5c2bb42df4cc80a7a6bdc6ba52
NDK1410 <khanh.nd1410@gmail.com>
2019-04-19 13:18:16 +0700

    devise-cancancan

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize user
+    user ||= User.new
+
+    case user.role
+    when "admin"
+      can :manage, :all
+    when "member"
+      can :read, :all
+      can :create, :lesson, :user_id => user.id
+      can :update, :lesson, :user_id => user.id
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :lessons
   has_many :categories, through: :lessons
   has_many :learned_words
+
+  def role_enum
+    ["admin", "member"]
+  end
 end
