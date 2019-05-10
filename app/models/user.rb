4cdc580b21eea31c095afc0029c53a95eff28365
NDK1410 <khanh.nd1410@gmail.com>
2019-05-10 18:57:50 +0700

    user-avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :lessons
   has_many :categories, through: :lessons
   has_many :learned_words
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
 
   def role_enum
     ["admin", "member"]
