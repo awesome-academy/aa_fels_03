29ede91984f981529a465aea45ce46b93ba348af
NDK1410 <khanh.nd1410@gmail.com>
2019-05-30 16:11:01 +0700

    action-cable

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,11 @@
 class Lesson < ApplicationRecord
+  after_create_commit :notify
   belongs_to :user
   belongs_to :category
   has_many :lesson_words
+
+  private
+  def notify
+  	Notification.create(event: "New Notification (#{self.body})")
+  end
 end
