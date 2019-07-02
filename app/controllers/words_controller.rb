09eadc2487b3fd831a22d6343daf4bcef55512a8
NDK1410 <khanh.nd1410@gmail.com>
2019-07-02 12:53:46 +0700

    filter-by-learned-words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,6 +2,9 @@ class WordsController < ApplicationController
   def index
     if params[:filter_form] == (t".c")
       @categories = Category.all
+    elsif params[:filter_form] == (t".lr")
+      all_lessons = current_user.lessons
+      @unlearned_words = Word.urlearned_words(all_lessons.ids)
     else
       @words = Word.all
     end
